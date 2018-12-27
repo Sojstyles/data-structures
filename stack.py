a8d6dee64f09383f173011700e1e955401fe7d67
Edison Er <edisoner@bu.edu>
2018-12-27 17:55:37 -0500

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,34 +1,40 @@
    
   # STACK
 
+
 class Stack:
 # LIFO data structure; last element of array is first one popped
     
     def __init__(self):
         self.stack = []
 
+      
     def check_len(self):
         return len(self.stack)
 
+      
     def push(self, element):
         """Pushes an element onto the stack (at the end of the array)"""
+         
         self.stack += [element]
 
+      
     def pop(self):
         """Removes and returns the top element of the stack"""
+         
         if len(self.stack) > 0:
             return self.stack.pop()
         print("No items on the stack to pop")
 
+      
     def peek(self):
         """Returns the top element of the stack without removing it"""
+         
         if len(self.stack) > 0:
             return self.stack[len(self.stack) - 1]
         print("No items on the stack")
 
-#------------------------------------------------------#
-
-# Examples:
+#-----------------------------------------------------------------------------------------------------------------#
 
 sample_stack = Stack()
 sample_stack.push(5), sample_stack.push(6), sample_stack.push(1)
