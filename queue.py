42cfeff08e1f4a44faf40917f260128eccd03ac2
Edison Er <edisoner@bu.edu>
2018-12-27 17:54:18 -0500

    Update queue.py

--- queue.py
+++ queue.py
@@ -4,28 +4,33 @@ class Queue:
     def __init__(self):
         self.queue = []
 
+        
     def check_len(self):
         return len(self.queue)
 
+    
     def enqueue(self, element):
         """Inserts the element at the beginning of the queue"""
+        
         self.queue.insert(0, element)
 
+        
     def dequeue(self):
         """Removes the last element of the queue and returns it"""
+        
         if len(self.queue) > 0:
             return self.queue.pop()
         print("No items in the queue")
 
+        
     def peek(self):
         """Returns the next element to be dequeued without removing it"""
+        
         if len(self.queue) > 0:
             return self.queue[len(self.queue) - 1]
         print("No items in the queue")
 
-#------------------------------------------------------#
-
-# Examples:
+#-----------------------------------------------------------------------------------------------------------------#
 
 sample_queue = Queue()
 sample_queue.enqueue(5), sample_queue.enqueue(6), sample_queue.enqueue(1)
